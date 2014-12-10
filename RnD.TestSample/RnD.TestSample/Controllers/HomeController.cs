f8e81d19debb678e01adf17b6272e575629dca18
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2014-12-10 17:41:26 +0600

    2014-12-10

---
+++ RnD.TestSample/RnD.TestSample/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using RnD.TestSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RnD.TestSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            Profile profile = new Profile();
+            return View(profile);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Profile profile)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAjax(Profile profile)
+        {
+            return View();
+        }
+    }
+}
---
+++ RnD.TestSample/RnD.TestSample/Models/AppModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace RnD.TestSample.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        [DisplayName("Name: ")]
+        public string  Name { get; set; }
+        [DisplayName("Email: ")]
+        public string Email { get; set; }
+        public Address Address { get; set; }
+        public Contact Contact { get; set; }
+
+    }
+
+    public class Address
+    {
+        public int Id { get; set; }
+        [DisplayName("Address: ")]
+        public string Name { get; set; }
+    }
+
+    public class Contact
+    {
+        public int Id { get; set; }
+        [DisplayName("Contact Number: ")]
+        public string Number { get; set; }
+    }
+}
